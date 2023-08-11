bf74831333159acf5569a023af9ff67a82fd17cf
icsbariboa <icsbariboa@gmail.com>
2023-08-11 08:48:32 +0300

    removed unnecessary lines moved lines of code into one line when was viable

--- CurrencyRouletteGame.py
+++ CurrencyRouletteGame.py
@@ -2,13 +2,10 @@ import random
 from currency_converter import CurrencyConverter
 from Live import check_input_int
 
-CONVERTOR = CurrencyConverter()
-
 
 def get_money_info():
     random_usd = random.randrange(1, 100)
-    value_in_ils = int(CONVERTOR.convert(random_usd, "USD", "ILS"))
-    return random_usd, value_in_ils
+    return random_usd, int(CurrencyConverter().convert(random_usd, "USD", "ILS"))
 
 
 def get_guess_from_user():
@@ -17,7 +14,5 @@ def get_guess_from_user():
 
 def currency_roulette_game_play(difficulty):
     value_in_usd, value_in_ils = get_money_info()
-    min_answer, max_answer = (value_in_ils - (5 - difficulty)), (value_in_ils + (5 - difficulty))
     print(f"How much {value_in_usd} dollars are worth in shekels?")
-    user_guess = get_guess_from_user()
-    return min_answer <= user_guess <= max_answer
+    return (value_in_ils - (5 - difficulty)) <= get_guess_from_user() <= (value_in_ils + (5 - difficulty))
