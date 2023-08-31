dd87821a89fb71889352300530e564748e5628e9
ShayH <hardufshay@gmail.com>
2023-08-31 21:42:18 +0300

    Added a requirements file. Added the use of HTML templates for the score server

--- MainScores.py
+++ MainScores.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask("ScoreServer")
 
@@ -9,7 +9,7 @@ def score_server():
         score_file = open("Scores.txt")
         current_score = score_file.read()
         score_file.close()
-        return f"<html><head><title>Scores Game</title></head><body><h1>The score is: <div id='score'>{current_score}</div></h1></body></html>"
+        return render_template("scores_page.html", current_score=current_score)
 
 
 app.run(host="0.0.0.0", port=5001, debug=False)
