5ba37a91bee423ed53e1ad3d81201dd1e9151720
icsbariboa <icsbariboa@gmail.com>
2023-09-23 10:10:56 +0300

    Added a potential fix for selenium on multiple chrome versions.

--- e2e.py
+++ e2e.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 from constants import ipaddress_score_server, port_score_server
+from selenium.webdriver.chrome.service import Service
+
+service = Service()
+options = webdriver.ChromeOptions()
 
 
 def test_scores_service(app_url):
-    driver1 = webdriver.Chrome()
+    driver1 = webdriver.Chrome(service=service, options=options)
     driver1.get(app_url)
     score = driver1.find_element("id", "score")
     return 0 <= int(score.text) <= 1000
